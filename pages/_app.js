838681d0b5eac89ac9beba21a09c0dc9e5666120
Mike Harper <mike@mcharper.com>
2021-09-17 14:32:05 +0100

    Further work on card styling. Catch up content management metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles.css'
-import Layout from '../components/layout'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+
+import Layout from '../components/layout'
+import '../styles.css'
+
 config.autoAddCss = false
 
 export default function MyApp({Component, pageProps}) {
--- pages/projects.js
+++ pages/projects.js
@@ -1,35 +1,33 @@
 import Head from "next/head"
 import { Component } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faBuilding, faCalendarPlus, faChartPie, faDoorOpen, faFileContract, faFileDownload, faHamburger, faHandHolding, faHome, faHouseUser, faLaptopHouse, faMapPin, faPizzaSlice, faPlusSquare, faTruck, faUserPlus } from "@fortawesome/free-solid-svg-icons";
-import Link from 'next/link'; 
 import dayjs from 'dayjs'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarPlus, faChartPie, faFileContract, faHamburger, faMapPin, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 
-import { attributes, react as ProjectsContent } from '../content/projects.md';
-import { faLinkedinIn, faServicestack } from "@fortawesome/free-brands-svg-icons";
+import { attributes } from '../content/projects.md';
 
-export default class ProjectsAlt extends Component {
+export default class Projects extends Component {
   render() {
-    let { preamble, projects } = attributes;
+    let { projects } = attributes;
 
     return (
-        <div className={"page"}>
+      <div className={"page"}>
         <Head>
           <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         </Head>
 
         <div className={"container_grid"}>
 
-          {projects.map((project, k) => (
-            <div className="card card__para">
-              <div>
-                <h3>
+          {projects.map((project) => (
+            <div className="card">
+              <div className="card__header">
+                <h2>
                 {`${project.organisation}`}
-                  <span style={{fontWeight: "400", float: "right", marginRight: "1rem"}}>
+                  <span className={"card-top-corner"}>
                     {dayjs(project.start).format('MMM YY')} - {dayjs(project.end).format('MMM YY')}
                   </span>
-                </h3>
-                <h3 style={{fontSize: "1.25rem", marginBottom: "0.25rem", fontWeight: "400", backgroundColor: "silver", borderRadius: "0.25rem", color: "var(--clr-dark)", textAlign: "center"}}>
+                </h2>
+                <h3>
                 {project.name}
                 </h3>
               </div>
@@ -64,14 +62,9 @@ export default class ProjectsAlt extends Component {
               </ul>
 
               <img className={"card__img"} src={`/img/projects/${project.image}.jpg`} />
-{/* 
-              <p style={{fontSize: "0.9rem", fontStyle: "italic"}}>
-                <img className={"card__img"} style={{width: "50%", float: "left", border: "0.1rem solid var(--clr-subdued)", margin: "0.25rem"}} src={`/img/projects/${project.image}.jpg`} />
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aamet consectetur adipisicing elit sdf sdf.
-              </p> */}
 
-              <p style={{fontSize: "1rem", clear: "both"}}>
-              {project.synopsis}
+              <p>
+                {project.synopsis}
               </p>
 
               <ul className={"skillList"}>
