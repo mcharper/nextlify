2ef0a7d75021198b3b4214475cd458f0366b8c11
Mike Harper <mike.harper@agoradcm.com>
2022-03-30 22:46:18 +0100

    Add favicons and a web page title

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Head from "next/head"
+
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
@@ -8,7 +10,18 @@ config.autoAddCss = false
 
 export default function MyApp({Component, pageProps}) {
     return (
-    <Layout>
-        <Component {...pageProps} />
-    </Layout>)
+        <>
+            <Head>
+                <title>Mike Harper - IT Contract Developer</title>
+                <meta name="description" content="A versatile full-stack developer with over 20 years' industry experience, currently working with React and TypeScript."></meta>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    )
 }
